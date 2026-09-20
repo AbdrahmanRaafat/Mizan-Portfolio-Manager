d73ac0b435f8e09beb6c8fead11fb0e1695fb6f8
AbdrahmanRaafat <74067572+AbdrahmanRaafat@users.noreply.github.com>
2026-09-21 02:17:28 +0300

    Enable full functionality in demo mode

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 mkdir -p server
 cat bundle/part-*.b64 | tr -d '\r\n' | base64 -d > server/index.js.xz
-echo "b4dcafb306382e9dde4d3606913b555999ccf17eebff77edc802515e94995ddf  server/index.js.xz" | sha256sum -c -
+echo "169d96bf408c68f47b2429c48a81725fc50c52504f22de3638121ba24afacd38  server/index.js.xz" | sha256sum -c -
 if command -v xz >/dev/null 2>&1; then
   xz -d -f server/index.js.xz
 elif command -v python3 >/dev/null 2>&1; then
